da72c3bbe0befbf91e5b265b4bde3aba3fd9592f
Mauro <mauro.fonseca@outlook.com.ar>
2020-11-15 10:11:04 -0300

    FINALMENTE FUNCIONA! El esclavo recibe el tipo de mensaje apropiadamente y puede mostrarlo en la consola sin fallas. El esclavo clasifica el mensaje y elabora un tipo de mensaje de respuesta (solo probado con RESPONDER_PROM pero esta al fondo del anidamiento) y le respodne al amo. El amo toma la respuesta y muestra el primer caracter por pantalla.

--- slave_receiver/slave_receiver.ino
+++ slave_receiver/slave_receiver.ino
@@ -6,79 +6,87 @@
 #define CARACTER_SEPARADOR '/'
 
 uint8_t tamano_mensaje;
-uint8_t tipo_mensaje[14];
-float fl1 = 14.25;
-uint8_t f1[4];
-uint8_t f2[4];
-uint8_t f3[4];
-uint8_t f4[4];
+char* tipo_mensaje_recibido;
+char payload[20];
+
+//Recibe un puntero donde escribira el tipo de mensaje de la respuesta
+  //y un puntero que apunta al payload
+char* seleccionar_tipo_respuesta(){
+  char* tipo_recibido=(char*)tipo_mensaje_recibido;
+  char* toReturn;
+  float f1 = 19.25;
+  
+  if(strcmp("OBTENER_TEMP",tipo_recibido) == 0){
+    toReturn = "AESPONDER_TEMP";
+  }else{
+    if(strcmp("OBTENER_MIN",tipo_recibido) == 0){
+      toReturn = "BESPONDER_MIN";
+    }else{
+      if(strcmp("OBTENER_MAX",tipo_recibido) == 0){
+        toReturn = "CESPONDER_MAX";
+      }else{
+        if(strcmp("OBTENER_PROM",tipo_recibido) == 0){
+          toReturn = "DESPONDER_PROM";
+        }else{
+          toReturn = "EESPONDER_TODO";
+        }
+      }
+    }
+  }
+  return toReturn;
+}
+
 
 
 // Ejecuta cada vez que recibe datos del amo
 // Es un evento, se debe asociar en el setup con Wire.onReceive()
 void receiveEvent(int howMany) {
   uint8_t i = 0;
-  uint8_t c = NULL;
+  char c = NULL;
 
   //Tomo el tamano del mensaje
   if (Wire.available() > 3){  //Tengo 4 caracteres para leer
     c = Wire.read();  //Leo el caracter inicial
     tamano_mensaje = Wire.read(); //leo el tamano del mensaje
     c = Wire.read();  //Tomo el CARACTER_SEPARADOR
-    c = Wire.read();  //Tomo el primer caracter de tipo_mensaje
+    c = Wire.read();  //Tomo el primer caracter de tipo_mensaje_recibido
   }
-
   //Tomo el tipo de mensaje
   while(Wire.available()>0 && c!=CARACTER_SEPARADOR){
-    tipo_mensaje[i]= (char) c;
-    c = Wire.read();
+    tipo_mensaje_recibido[i]= (char) c;
+    c = (char) Wire.read();
     i++;
   }
-
+  tipo_mensaje_recibido[i] = '\0';
+  
   //Estoy apuntando al caracter separador
   if(Wire.available() > 0){
     c = Wire.read();  //Salteo el CARACTER_SEPARADOR y apunto al primer caracter del payload
   }
 
-  i=0;
-
-  Serial.print(tamano_mensaje);
-  Serial.print(' ');
-  while(tipo_mensaje[i]!=NULL){
-    Serial.print((char)tipo_mensaje[i]);
-    i++;
-  }
-  Serial.print(' ');
-  Serial.println(f1[0]);/*
-  Serial.print(f2);
-  Serial.print(f3);
-  Serial.println(f4);*/
+  Serial.println(tipo_mensaje_recibido);
+  
 }
 
 void requestEvent() {
   uint8_t i = 0;
-
-  //Serial.println(tamano_mensaje);
-  //Wire.write(tamano_mensaje); // Respuesta enviada al amo
-  //Considerar meter todo en un gran buffer de uint8_t llamada mensaje y llamar solo una vez a Wire.write
+  char* tipo_mensaje_respuesta;
+  
   Wire.write('*');
   Wire.write(tamano_mensaje);
   Wire.write('/');
-    while((i<sizeof(tipo_mensaje))){
-    Wire.write(tipo_mensaje[i]);
-    i++;
-  }
+  tipo_mensaje_respuesta = seleccionar_tipo_respuesta();
+  //Serial.println(tipo_mensaje_respuesta);
+  Wire.write(tipo_mensaje_respuesta);
   Wire.write('/');
-  Wire.write(f1,4);/*
-  Wire.write(f2);
-  Wire.write(f3);
-  Wire.write(f4);*/
+  //ACa irian los write de los floats
   Wire.write('*');
 }
 
 
 
 void setup() {
+  tipo_mensaje_recibido = (char *) malloc(13*sizeof(char));
   Wire.begin(8);                // Me uno al I2C con direccion 8
   Wire.onReceive(receiveEvent); // Registro funciones a eventos
   Wire.onRequest(requestEvent);
