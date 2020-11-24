8b2db6d8166f23830069cc0ac6917319f39e8472
Mauro <mauro.fonseca@outlook.com.ar>
2020-11-24 11:52:54 -0300

    Finalizacion de la actividad 2.

--- Labo05/Lab05_Act02/Lab05_Act02.ino
+++ Labo05/Lab05_Act02/Lab05_Act02.ino
@@ -1,67 +1,86 @@
 #include <Arduino_FreeRTOS.h>
+#include <timers.h>
 
-#define FRECUENCIA1 1000
-#define FRECUENCIA2 250
-#define LED1 13
-#define LED2 5
-
-// define two tasks for Blink
-void TaskBlink1(void *pvParameters);
-void TaskBlink2(void *pvParameters);
+#define FRECUENCIA4HZ 250     //4Hz
+#define FRECUENCIA2HZ 500     //2Hz
+#define FRECUENCIA1HZ 1000    //1Hz
+#define FRECUENCIA05HZ 2000   //0.5Hz
+#define IDBLINK1 1
+#define IDBLINK2 2
 
 void setup() {
   Serial.begin(9600);
   while (!Serial) {
   }
-  pinMode(LED1, OUTPUT);
-  pinMode(LED2, OUTPUT);
 
-  // Now set up two tasks to run independently.
-  xTaskCreate(
-    TaskBlink1
-    ,  "Blink1"   // A name just for humans
-    ,  128  // This stack size can be checked & adjusted by reading the Stack Highwater
-    ,  NULL
-    ,  2  // Priority, with 3 (configMAX_PRIORITIES - 1) being the highest, and 0 being the lowest.
-    ,  NULL );
+  //Seteo los pines de los leds como salida
+  DDRB |= (1<<DDB5);
+  DDRD |= (1<<DDD5);
+  
+  //Creo manejadores de timers
+  TimerHandle_t handlerBlink1 = xTimerCreate(
+                                  "Blink1",
+                                  pdMS_TO_TICKS(FRECUENCIA4HZ),
+                                  pdTRUE,  //Asi el timer se reinicia solo
+                                  IDBLINK1,
+                                  callbackTimer1);
+  TimerHandle_t handlerBlink2 = xTimerCreate(
+                                  "Blink2",
+                                  pdMS_TO_TICKS(FRECUENCIA1HZ),
+                                  pdTRUE,  //Asi el timer se reinicia solo
+                                  IDBLINK2,
+                                  callbackTimer2);
 
-  xTaskCreate(
-    TaskBlink2
-    ,  "Blink2"
-    ,  128  // Stack size
-    ,  NULL
-    ,  1  // Priority
-    ,  NULL );
+  if(handlerBlink1 != NULL){
+      if(xTimerStart(handlerBlink1, 0) != pdPASS){
+        Serial.println("Fallo el blink1");
+      }
+  }
+  if(handlerBlink2 != NULL){
+      if(xTimerStart(handlerBlink2, 0) != pdPASS){
+        Serial.println("Fallo el blink2");
+      }
+  }
 
-  // Now the task scheduler, which takes over control of scheduling individual tasks, is automatically started.
+  //Inicio los timers. Si no hay RAM suficiente para reservarlos se pasa al siguiente comando
+  vTaskStartScheduler();
+  Serial.println("RAM insuficiente");
 }
 
 void loop(){
-  // Empty. Things are done in Tasks.
 }
 
 /*--------------------------------------------------*/
-/*---------------------- Tasks ---------------------*/
+/*-------------------- Callbacks -------------------*/
 /*--------------------------------------------------*/
 
-void TaskBlink1(void *pvParameters){
-  (void) pvParameters;
-  
-  while(1){
-    digitalWrite(LED1, HIGH);
-    vTaskDelay(FRECUENCIA1 / portTICK_PERIOD_MS );
-    digitalWrite(LED1, LOW);
-    vTaskDelay(FRECUENCIA1 / portTICK_PERIOD_MS );
-  }
+void callbackTimer1(TimerHandle_t xTimer){
+  PORTB ^= (1<<PB5);
 }
 
-void TaskBlink2(void *pvParameters){
-  (void) pvParameters;
+void callbackTimer2(TimerHandle_t xTimer){
+  char opcion = 0;
+
+  PORTD ^= (1<<PD5);
   
-  while(1){
-    digitalWrite(LED2, HIGH);
-    vTaskDelay(FRECUENCIA2 / portTICK_PERIOD_MS );
-    digitalWrite(LED2, LOW);
-    vTaskDelay(FRECUENCIA2 / portTICK_PERIOD_MS );
+  if(Serial.available()>0){
+    opcion = Serial.read();
+    switch(opcion){
+      case '1':
+        xTimerChangePeriod(xTimer,
+                           pdMS_TO_TICKS(FRECUENCIA05HZ),
+                           0);
+        break;
+      case '2':
+        xTimerChangePeriod(xTimer,
+                           pdMS_TO_TICKS(FRECUENCIA2HZ),
+                           0);
+        break;
+       default:
+        xTimerChangePeriod(xTimer,
+                           pdMS_TO_TICKS(FRECUENCIA1HZ),
+                           0);
+        break;
+    }
   }
 }
